3d80336bca567c9774bd019171f8fb522de7894d
seanrivardmorton <sean.rivard.morton@gmail.com>
2024-09-06 18:34:55 +0100

    Added curl examples for testing API, implemented endpoints

--- src/app/api/entries/[id]/route.ts
+++ src/app/api/entries/[id]/route.ts
@@ -1,8 +1,9 @@
 import db from "@/db"
 import { entries } from "@/entries"
 import { eq } from "drizzle-orm"
+import { z } from "zod"
 
-export async function GET(request: Request, { params }: { params: { slug: string }) {
+export async function GET(request: Request, { params }: { params: { slug: string } }) {
   console.log(`GET entries/${params.slug}`)
 
   const entry = await db.select().from(entries).where(eq(entries.id, 1))
@@ -12,4 +13,24 @@ export async function GET(request: Request, { params }: { params: { slug: string
   }
 
   return Response.json({ entry })
+}
+
+
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+
+  console.log(`DELETE entries/${params.id}`)
+
+  const id = z.coerce.number().parse(params.id)
+
+  const result = await db.delete(entries).where(eq(entries.id, id))
+
+  if (!result) {
+    return Response.json({ message: "Error" }, { status: 404 })
+  }
+
+  if (result.length < 1) {
+    return Response.json({ message: "No entries found" }, { status: 404 })
+  }
+
+  return Response.json({ message: "Deleting Entries" });
 }
--- src/app/api/entries/route.ts
+++ src/app/api/entries/route.ts
@@ -1,9 +1,44 @@
+import db from "@/db";
+import { entries } from "@/entries";
+import { request } from "http";
+
 export async function GET() {
   console.log('GET ENTRIES')
-  return Response.json({ message: "Getting Entries" });
+
+  const result = await db.select().from(entries)
+
+  if (!result) {
+    return Response.json({ message: "Error" }, { status: 404 })
+  }
+
+  if (result.length < 1) {
+    return Response.json({ message: "No entries found" }, { status: 404 })
+  }
+
+  return Response.json({ status: 200, result });
 }
 
-export async function POST() {
-  console.log('POST ENTRIES')
-  return Response.json({ message: "Posting Entries" });
+export async function POST(request: Request) {
+  console.log('POST /entries')
+
+  const body = await request.json()
+
+  const result = await db.insert(entries).values({
+    title: body.title,
+    content: body.content,
+  })
+
+  if (!result) {
+    return Response.json({ message: "Error" }, { status: 404 })
+  }
+
+  return Response.json({ status: 200, result });
+}
+
+
+export async function DELETE(request: Request, params: { id: string }) {
+  const id = parseInt(params.id)
+  console.log('DELETE /entries', id)
+
+  return Response.json({ message: "Deleting Entries" });
 }
